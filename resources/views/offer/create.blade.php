ba45704b7ed3dd4b707b4396ecc2c731287fe6b2
Patryk Pawłowicz <patryk.pawlowicz@wp.pl>
2020-12-16 10:10:53 +0100

    Co-authored-by: Justyna Gapys <j.gapys.162@studms.ug.edu.pl> Co-authored-by: Aleksandra Okrój <a.okroj.127@studms.ug.edu.pl>

--- resources/views/offer/create.blade.php
+++ resources/views/offer/create.blade.php
@@ -1,17 +1,20 @@
+@extends('layouts.default')
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Create offer</title>
 </head>
 <body>
     <form method="POST">
         @csrf
+        Name:
         <input type="text" name="name">
+        Description:
         <input type="text" name="description">
-        <input type="submit" value="create!">
+        <input type="submit" value="Create!">
     </form>
 </body>
 </html>
